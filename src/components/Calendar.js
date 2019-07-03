de2d6e80f1db359ada6c2f20152dd5d22c2c7cbf
Andreas <AndrewNaTesla@gmail.com>
2019-07-03 21:38:29 +0300

    Fix modal in Calendar
    Now app does not crash when a day is clicked in the calendar.

--- src/components/Calendar.js
+++ src/components/Calendar.js
@@ -68,7 +68,7 @@ function Calendar(props) {
         handleSubmit={selectedCook => submitHandler(selectedCook)}
         open={modalVisibility}
         onClose={_ => setModalVisibility(false)}
-        cooks
+        cooks={cooks}
       >
         The cook on {selectedDate}
       </Modal>
